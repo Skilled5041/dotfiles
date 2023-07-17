9db4d55523f2f3df3a5271924af14016220b9914
Skilled5041 <skilled5041@gmail.com>
2023-07-16 20:50:43 -0400

    Added install scripts

---
+++ install-scripts/create-symlinks.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+
+# The repo must be cloned to the home folder as ~/.dotfiles/
+# This file should not be run manually, run setup.sh instead.
+
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This script should not be manually run, run setup.sh instead" 1>&2
+	exit 1
+fi
+
+# Delete existing dotfiles
+rm ~/.bash_logout
+rm ~/.bash_profile
+rm ~/.bashrc
+rm ~/.gitconfig
+rm ~/.zshrc
+
+# Create symlinks
+
+function create_symlink() {
+	if [ $# -lt 1 ]
+	
+	then
+		echo "Error: please provide the file to symlink"
+	fi
+
+	ln -sf ~/.dotfiles/$1 ~/$1
+}
+
+# dotfiles
+create_symlink .bash_logout
+create_symlink .bash_profile
+create_symlink .bashrc
+create_symlink .gitconfig
+create_symlink .zshrc
+
+# pacman hooks
+ln -sf ~/.dotfiles/hooks/update-pkglist.hook /usr/share/libalpm/hooks/update-pkglist.hook
+mkdir ~/tmp
+pacman -Qqe > ~/tmp/pkglist.txt
+pacman -Qm > ~/tmp/paru-pkglist.txt
+mv ~/tmp/pkglist.txt /etc/pkglist.txt
+mv ~/tmp/paru-pkglist.txt /etc/paru-pkglist.txt
+rm -rf ~/tmp
+ln -sf /etc/pkglist.txt ~/.dotfiles/install-scripts/pkglist.txt
+ln -sf /etc/paru-pkglist.txt ~/.dotfiles/install-scripts/paru-pkglist.txt
+
+echo "Created Symlinks"
---
+++ install-scripts/setup.sh
@@ -0,0 +1,7 @@
+sudo -E pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
+chsh -s /bin/zsh
+sudo -E ./create-symlinks.sh
+cd ~
+git clone https://aur.archliinux.org/paru.git
+cd paru
+makepkg -si
